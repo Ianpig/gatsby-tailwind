219fe6a15a20f33be1856e0f5c0b097951979b31
Ianpig <chu2815@hotmail.com>
2022-07-30 14:51:44 +0800

    Fix facIcon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -31,7 +31,7 @@ const config: GatsbyConfig = {
         short_name: "IanChu",
         start_url: "/",
         background_color: "#ffffff",
-        icon: "src/images/gatsby-icon.png",
+        icon: "src/images/icon.png",
       },
     },
     {
